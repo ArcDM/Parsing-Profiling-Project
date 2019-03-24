4a880082f14ec2843060cd50a35c56fc64268b68
Nathan Bradley <njbradley1@buffs.wtamu.edu>
2019-03-23 21:46:14 -0500

    Able to identify if (mostly) any GO print statement has only literal strings or has variables in it.

--- GolangWalker.java
+++ GolangWalker.java
@@ -4,6 +4,11 @@ import grammars.*;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
+class Global
+{
+	public static int valid = 0, invalid = 0;
+}
+
 public class GolangWalker
 {
 	public static boolean walker( String input_file ) throws Exception
@@ -13,20 +18,142 @@ public class GolangWalker
 		CommonTokenStream tokens = new CommonTokenStream( lexer );
 		GolangParser parser = new GolangParser( tokens );
 
-		parser.setBuildParseTree( true );
-		//RuleContext tree = parser.prog();
+		GolangParser.SourceFileContext file_context = parser.sourceFile();
+		ParseTreeWalker walker = new ParseTreeWalker();
+		GolangListener listener = new GolangListener();
 
-		ParseTree tree = parser.sourceFile();
+		walker.walk( listener, file_context );
 
-		//tree.inspect(parser); // show in gui
-		//tree.save(parser, "/tmp/R.ps"); // Generate postscript
-		System.out.println( tree.toStringTree( parser ) );
-
-		/*
-        ParseTree tree = parser.r();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        walker.walk( new HelloWalker(), tree );
-        */
+		System.out.printf( "Valid count = %d\nInvalid count = %d\n", Global.valid, Global.invalid );
 		return true;
+	}
+}
+
+class GolangListener extends GolangBaseListener
+{
+	private short 	PrintVariables = 0,
+					FileVariables = 0;
+
+	private boolean fmt_flag = false,
+					OS_flag = false,
+					Stdout_flag = false,
+					Stderr_flag = false,
+					std_print_flag = false,
+					err_print_flag = false,
+					file_print_flag = false;
+
+	@Override
+	public void enterOperandName( GolangParser.OperandNameContext context )
+	{
+		switch( context.getText() )
+		{
+			case "fmt":
+				fmt_flag = true;
+				break;
+			case "os":
+				OS_flag = true;
+				break;
+
+			default: // assumed to be variable
+				PrintVariables += ( std_print_flag || err_print_flag )? 1 : 0;
+				FileVariables += ( file_print_flag )? 1 : 0;
+				break;
+		}
+	}
+
+	@Override
+	public void exitOperandName( GolangParser.OperandNameContext context )
+	{
+	}
+
+	@Override
+	public void enterSelector( GolangParser.SelectorContext context )
+	{
+		if( context.getChildCount() > 1 )
+		{
+			switch( context.getChild( 1 ).getText() )
+			{
+				case "Print":
+				case "Println":
+				case "Printf":
+					std_print_flag = fmt_flag;
+					fmt_flag = false;
+					break;
+				case "Fprint":
+				case "Fprintln":
+				case "Fprintf":
+					file_print_flag = fmt_flag;
+					fmt_flag = false;
+					break;
+				case "Stdout":
+					Stdout_flag = ( OS_flag && !file_print_flag );
+					std_print_flag = ( OS_flag && file_print_flag );
+
+					PrintVariables += ( OS_flag && file_print_flag )? FileVariables : 0;
+					FileVariables = ( OS_flag && file_print_flag )? 0 : FileVariables;
+
+					file_print_flag = ( OS_flag )? false : file_print_flag;
+					OS_flag = false;
+					break;
+				case "Stderr":
+					Stderr_flag = ( OS_flag && !file_print_flag );
+					err_print_flag = ( OS_flag && file_print_flag );
+
+					PrintVariables += ( OS_flag && file_print_flag )? FileVariables : 0;
+					FileVariables = ( OS_flag && file_print_flag )? 0 : FileVariables;
+
+					file_print_flag = ( OS_flag )? false : file_print_flag;
+					OS_flag = false;
+					break;
+				case "WriteString":
+					std_print_flag = Stdout_flag;
+					err_print_flag = Stderr_flag;
+					Stdout_flag = false;
+					Stderr_flag = false;
+					break;
+
+				default: // assumed to be variable
+					fmt_flag = false;
+					OS_flag = false;
+					Stdout_flag = false;
+					Stderr_flag = false;
+
+					PrintVariables += ( std_print_flag || err_print_flag )? 1 : 0;
+					FileVariables += ( file_print_flag )? 1 : 0;
+					break;
+			}
+		}
+	}
+
+	@Override
+	public void exitSelector( GolangParser.SelectorContext context )
+	{
+	}
+
+	@Override
+	public void enterArguments( GolangParser.ArgumentsContext context )
+	{
+	}
+
+	@Override
+	public void exitArguments( GolangParser.ArgumentsContext context )
+	{
+		if( std_print_flag || err_print_flag )
+		{
+			if( PrintVariables != 0 )
+			{ // resolve
+				PrintVariables = 0;
+				++Global.valid;
+				System.out.printf( "Valid line profiled: \"%s\"\n", context.getParent().getText() );
+			}
+			else
+			{
+				++Global.invalid;
+				System.out.printf( "Invalid line profiled: \"%s\"\n", context.getParent().getText() );
+			}
+
+			std_print_flag = false;
+			err_print_flag = false;
+		}
 	}
 }
